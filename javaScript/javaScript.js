9e49273342bb73aaec26254bcae1d9ad66ae6338
perezitoPike <wilderchur@gmail.com>
2025-12-04 19:30:06 -0400

    UPD: Mejoras en animaciones de botones, inputs e imagenes

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,3 +1,10 @@
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 1) {
+        header.classList.add("scroll-activo");
+    }
+    else header.classList.remove("scroll-activo");
+});
 
 const loginSection = document.getElementById('login-section');
 const registerSection = document.getElementById('register-section');
@@ -5,14 +12,18 @@ const registerSection = document.getElementById('register-section');
 const showRegisterLink = document.getElementById('show-register');
 const showLoginLink = document.getElementById('show-login');
 
-showRegisterLink.addEventListener('click', function (e) {
-    e.preventDefault(); // Evita que recargue la página
-    loginSection.style.display = 'none';
-    registerSection.style.display = 'block';
-});
+if (showRegisterLink) {
+    showRegisterLink.addEventListener('click', function (e) {
+        e.preventDefault(); // Evita que recargue la página
+        loginSection.style.display = 'none';
+        registerSection.style.display = 'block';
+    });
+}
 
-showLoginLink.addEventListener('click', function (e) {
-    e.preventDefault();
-    registerSection.style.display = 'none';
-    loginSection.style.display = 'block';
-});
+if (showLoginLink) {
+    showLoginLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        registerSection.style.display = 'none';
+        loginSection.style.display = 'block';
+    });
+}
